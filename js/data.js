e974552a94929585892eba5369cc9920dcc66e63
Lada <lada.ovchinnikova@mail.ru>
2019-07-20 23:33:08 +0300

    исправления

--- js/data.js
+++ js/data.js
@@ -1,22 +1,27 @@
 'use strict';
 (function () {
 
-  var URL = 'https://js.dump.academy/kekstagram/datjja';
+  var URL = 'https://js.dump.academy/kekstagram/data';
 
-  window.data = function (onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+  window.data = {
+    loadPhotos: function (onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
 
-    xhr.open('GET', URL);
-    xhr.send();
+      xhr.open('GET', URL);
+      xhr.send();
+    }
   };
 
 })();
